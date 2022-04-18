ff900049bb0792bd639fc96fe21ba54b2bfbc67b
CharlieDaBru1990 <cgprins@gmail.com>
2022-04-18 10:05:10 +0200

    Second commit

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,9 @@
-document.write('Git is Awesome!')
+var customerName = prompt("Please enter your name", "<name goes here>");
+
+if (customerName!= null) {
+
+    document.getElementById("welcome").innerHTML =
+
+    "Hello " + customerName + "! How are you today?";
+
+}
